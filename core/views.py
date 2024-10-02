879259d05c91858f64ff25b5412de0bbad0451b6
Aryan Jain <aryanofficialjain@gmail.com>
2024-10-02 18:20:19 +0530

    account settings working fine with profile model tested

--- core/views.py
+++ core/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from .models import Profile
+from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -27,11 +29,18 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
+                # login in to the user 
+
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+
                 # Create a profile for the user
-                new_profile = Profile.objects.create(user=user, id_user=user.id)
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
 
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Passwords do not match')
             return redirect('signup')
@@ -41,5 +50,70 @@ def signup(request):
 
 def signin(request):
     
-    return render(request, 'signin.html')
-    
+    if(request.method=="POST"):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Creadientaisl Invalid')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+
+@login_required(login_url='signin')    
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        bio = request.POST.get('bio')
+        location = request.POST.get('location')
+
+        # Check if a new profile image is uploaded
+        if request.FILES.get('profileimage'):
+            user_profile.profileimg = request.FILES['profileimage']
+
+        user_profile.bio = bio
+        user_profile.location = location
+        user_profile.save()
+
+        messages.success(request, 'Profile updated successfully.')
+        return redirect('settings')  # Redirect to settings page after saving
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        if request.FILES.get('profileimage') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('profileimage')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signup')
