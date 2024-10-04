7639d4b2bb51a55247693729b0a6a0270ab434ea
Aryan Jain <aryanofficialjain@gmail.com>
2024-10-04 16:26:50 +0530

    follow and unfollow is completed

--- core/views.py
+++ core/views.py
@@ -1,14 +1,163 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from django.http import HttpResponse
+from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts })
+
+
+@login_required(login_url='signin')
+def upload(request):
+    if(request.method == "POST"):
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+        return redirect('/')
+    else:
+        return redirect('/')
+    
+
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get(post_id)
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+
+        post.no_of_likes = post.no_of_likes + 1
+        post.save()
+
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes -1
+        post.save()
+
+        return redirect("/")
+    
+
+@login_required(login_url='signin')    
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        bio = request.POST.get('bio')
+        location = request.POST.get('location')
+
+        # Check if a new profile image is uploaded
+        if request.FILES.get('profileimage'):
+            user_profile.profileimg = request.FILES['profileimage']
+
+        user_profile.bio = bio
+        user_profile.location = location
+        user_profile.save()
+
+        messages.success(request, 'Profile updated successfully.')
+        return redirect('settings')  # Redirect to settings page after saving
+
+        return render(request, 'setting.html', {'user_profile': user_profile})
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        if request.FILES.get('profileimage') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('profileimage')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
+
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signup')
+
+@login_required(login_url='signin')
+def profile(request, pk):
+    user_object = User.objects.get(username=pk)
+    user_profile = Profile.objects.get(user=user_object)
+    user_post = Post.objects.filter(user=pk)
+    user_post_length = len(user_post)
+
+    follower = request.user.username
+    user = pk
+
+    if FollowersCount.objects.filter(follower=follower, user=user):
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
+
+        user_followers = len(FollowersCount.objects.filter(user=pk))
+        user_following = len(FollowersCount.objects.filter(follower=pk))
+
+
+
+
+
+    context = {
+        'user_object': user_object,
+        'user_profile': user_profile,
+        'user_post': user_post,
+        'user_post_length': user_post_length,
+        'button_text': button_text,
+        'user_followers': user_followers,
+        'user_following': user_following
+    }
+    return render(request, 'profile.html', context)
+
+@login_required(login_url='signin')
+def follow(request):
+    if(request.method == "POST"):
+        follower = request.POST['follower']
+        user = request.POST['user']
+
+        if(FollowersCount.objects.filter(follower=follower, user=user).first()):
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+
+            return redirect('/profile/'+user)
+        else:
+            new_follower = FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/'+user)
+    else:
+        return redirect("/")
+
+
 
 
 def signup(request):
@@ -66,54 +215,3 @@ def signin(request):
         return render(request, 'signin.html')
 
 
-@login_required(login_url='signin')    
-def settings(request):
-    user_profile = Profile.objects.get(user=request.user)
-
-    if request.method == 'POST':
-        bio = request.POST.get('bio')
-        location = request.POST.get('location')
-
-        # Check if a new profile image is uploaded
-        if request.FILES.get('profileimage'):
-            user_profile.profileimg = request.FILES['profileimage']
-
-        user_profile.bio = bio
-        user_profile.location = location
-        user_profile.save()
-
-        messages.success(request, 'Profile updated successfully.')
-        return redirect('settings')  # Redirect to settings page after saving
-
-    return render(request, 'setting.html', {'user_profile': user_profile})
-    user_profile = Profile.objects.get(user=request.user)
-
-    if request.method == 'POST':
-        if request.FILES.get('profileimage') == None:
-            image = user_profile.profileimg
-            bio = request.POST['bio']
-            location = request.POST['location']
-
-            user_profile.profileimg = image
-            user_profile.bio = bio
-            user_profile.location = location
-            user_profile.save()
-
-        if request.FILES.get('image') != None:
-            image = request.FILES.get('profileimage')
-            bio = request.POST['bio']
-            location = request.POST['location']
-
-            user_profile.profileimg = image
-            user_profile.bio = bio
-            user_profile.location = location
-            user_profile.save()
-
-
-    return render(request, 'setting.html', {'user_profile': user_profile})
-
-
-@login_required(login_url='signin')
-def logout(request):
-    auth.logout(request)
-    return redirect('signup')
